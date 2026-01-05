6fd8b0380bfa7aaaea5f7e5d8770b12dabb64f93
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2026-01-05 17:25:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv('parkinsons.csv')
+df=df.dropna()
+df.head()
+
+selected_features = ['MDVP:Fo(Hz)', 'MDVP:Jitter(%)']
+output_feature = 'status'
+
+X=df[selected_features]
+y=df[output_feature]
+
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler()
+X=scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=8)
+model.fit(X_train,y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred=model.predict(X_test)
+acurracy=accuracy_score(y_test,y_pred)
+print(acurracy)
+
 
